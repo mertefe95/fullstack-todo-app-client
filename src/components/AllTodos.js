0c78464374b5a555987adafc6506b377cf0c4cdf
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-02-05 14:44:36 +0300

    chngs

--- src/components/AllTodos.js
+++ src/components/AllTodos.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Card from 'react-bootstrap/Card'
+import UserContext from "./context/UserContext";
 
 function AllTodos({ todos }) {
+  const { userData } = useContext(UserContext)
+
 	const sortedArray = todos.sort((a, b) => (a.userId > b.userId ? 1 : -1))
 	return (
 		<div className='all-todos'>
 			{sortedArray.map((todo, key) => (
 				<div className='todo-div' key={todo.userId}>
 					<Card
-						bg='primary'
+						bg={userData.user.id == todo.userid ? 'primary' : 'success'}
 						key={key}
 						text='white'
 						style={{ width: '18rem' }}
