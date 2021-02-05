edf59f74375cd9388a41d2d27d105414268032f2
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-02-05 14:58:58 +0300

    mcwqemf

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,35 +8,40 @@ function Header() {
 	const { userData, setUserData } = useContext(UserContext)
 
 	const handleData = async () => {
-		setUserData({
-			token: undefined,
-			user: undefined,
-		})
+		await setUserData({
+      token: null,
+      user: {
+        email: null,
+        username: null,
+        id: null
+      }})
+      await localStorage.setItem('auth-token', '')
+    
 	}
 
 	return (
 		<Navbar bg='light' variant='light'>
-			<Navbar.Brand href='#home'>Todo App</Navbar.Brand>
+			<Navbar.Brand href='/'>Todo App</Navbar.Brand>
 			<Nav className='mr-auto'>
 				<Nav.Link href='/'>VIEW ALL TODOS</Nav.Link>
 				<Nav.Link href='/mytodos'>VIEW ONLY MY TODOS</Nav.Link>
-				<Nav.Link href='#pricing'>Pricing</Nav.Link>
 			</Nav>
 			<Form inline>
-				{userData.user ? (
+				{userData.user.id === null ? (
+          <>
+						<Nav.Link href='/register'>REGISTER</Nav.Link>
+						<Nav.Link href='/login'>LOGIN</Nav.Link>
+					</>
+					
+				) : (
 					<>
 						<Nav.Link href='/add-todo'>ADD TODO</Nav.Link>
 						<button
 							style={{ background: 'none', border: 'none' }}
-							onClick={handleData}
+							onClick={() => handleData()}
 						>
-							<Nav.Link href='#'>LOGOUT</Nav.Link>
+							<Nav.Link href='/'>LOGOUT</Nav.Link>
 						</button>
-					</>
-				) : (
-					<>
-						<Nav.Link href='/register'>REGISTER</Nav.Link>
-						<Nav.Link href='/login'>LOGIN</Nav.Link>
 					</>
 				)}
 			</Form>
