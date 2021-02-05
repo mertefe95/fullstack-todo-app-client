6361cad38b3202ba82b6d928f9966e51746e018e
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-02-05 14:12:34 +0300

    chngs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from './components/layout/Footer'
+import Header from './components/layout/Header'
+import AllTodos from './components/AllTodos'
+import React, { useState, useEffect } from 'react'
+import UserContext from './components/context/UserContext'
+import Axios from 'axios'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Register from './components/Register'
+import Login from './components/Login'
+import AddTodo from './components/AddTodo'
+import MyTodos from './components/MyTodos'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState([])
+
+	const [userData, setUserData] = useState({
+		token: null,
+		user: {
+			id: null,
+			email: null,
+			username: null,
+		},
+	})
+
+	useEffect(() => {
+		const checkLoggedIn = async () => {
+			let token = localStorage.getItem('auth-token')
+			if (token === null) {
+				localStorage.setItem('auth-token', '')
+				token = ''
+			}
+
+			const userRes = await Axios({
+				method: 'post',
+				url: 'http://localhost:8080/users/tokenIsValid',
+				data: {},
+				headers: {
+					'x-auth-token': token,
+				},
+			})
+
+			setUserData({
+				token: token,
+				user: userRes.data,
+			})
+		}
+
+		checkLoggedIn()
+	}, [])
+
+	useEffect(() => {
+		Axios.get('http://localhost:8080/todos')
+			.then((res) => setTodos(res.data))
+			.catch((error) => console.log(error))
+	})
+
+	return (
+		<Router>
+			<UserContext.Provider value={{ userData, setUserData }}>
+				<Header />
+				<Switch>
+					<Route exact path='/' render={() => <AllTodos todos={todos} />} />
+					<Route exact path='/register' component={Register} />
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/add-todo' component={AddTodo} />
+					<Route
+						exact
+						path='/mytodos'
+						render={() => <MyTodos todos={todos} />}
+					/>
+				</Switch>
+				<Footer />
+			</UserContext.Provider>
+		</Router>
+	)
 }
 
-export default App;
+export default App
